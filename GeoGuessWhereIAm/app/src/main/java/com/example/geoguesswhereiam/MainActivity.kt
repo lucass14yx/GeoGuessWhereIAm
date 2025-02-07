351b9b56e117139f426ad0911077d182b9b12eb2
Lucas Serrano Pérez <lmserranop03@educastillalamancha.es>
2025-02-07 14:34:58 +0100

    Main Activity
    Modifico el Manifest para que arranque en Main Activity y configuro 4sec de espera hasta que te direcciona a Inicio

--- GeoGuessWhereIAm/app/src/main/java/com/example/geoguesswhereiam/MainActivity.kt
+++ GeoGuessWhereIAm/app/src/main/java/com/example/geoguesswhereiam/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.geoguesswhereiam
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.example.geoguesswhereiam.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val intent = Intent(this, Inicio::class.java)
+        lifecycleScope.launch {
+            delay(4000) // Espera 4 segundos
+            startActivity(intent)
         }
+
     }
 }
